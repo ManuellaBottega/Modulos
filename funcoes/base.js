39392a7adec14e9c697c66f3240e99a142ef44c9
Junior Carlos <junior-carlosdemattos@hotmail.com>
2025-08-04 11:00:59 -0300

    att base.js

--- funcoes/base.js
+++ funcoes/base.js
@@ -1,2 +1,9 @@
-// const menu = require('./funcoes/menu')
-// menu();
+const menu = require('./menu');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+menu(rl);
