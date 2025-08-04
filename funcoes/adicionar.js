74f33c5d0a337f7bc88c482b199c155db0b2fdc4
Junior Carlos <junior-carlosdemattos@hotmail.com>
2025-08-04 09:52:35 -0300

    Att. adicionar.js

--- funcoes/adicionar.js
+++ funcoes/adicionar.js
@@ -1,63 +1,40 @@
-const menuF = require("./menu");
-
-let missoes = [];
-let nomeAtual
-let destinoAtual
-let prioridadeAtual
-let tripulantesAtuais = []
-
-function adicionarMissaoAoGerenciador(rl) {
-
-    nomeAtual = '';
-    destinoAtual = '';
-    prioridadeAtual = 0;
-    tripulantesAtuais = [];
-
+function adicionarMissaoAoGerenciador(rl, missoes, menu) {
     rl.question('Digite o nome da missão: ', (nome) => {
-        nomeAtual = nome
         rl.question('Digite o destino da missão (ex: Marte, Júpiter, Saturno, etc.): ', (destino) => {
-            destinoAtual = destino
             rl.question('Digite a prioridade da missão de 1 a 5: ', (prioridade) => {
                 const prioridadeParsed = parseInt(prioridade);
                 if (isNaN(prioridadeParsed) || prioridadeParsed < 1 || prioridadeParsed > 5) {
                     console.log('Prioridade inválida! Por favor, digite um número entre 1 e 5.');
-                    adicionarMissaoAoGerenciador(rl); 
+                    adicionarMissaoAoGerenciador(rl, missoes, menu);
                 } else {
-                    prioridadeAtual = prioridadeParsed; 
-                    perguntarTripulantes(rl); 
+                    const tripulantes = [];
+                    perguntarTripulantes(rl, nome, destino, prioridadeParsed, tripulantes, missoes, menu);
                 }
-            })
-        })
-    })
+            });
+        });
+    });
 }
 
-    function perguntarTripulantes(rl) {
-            rl.question("Adicionar um tripulante: ", (tripulante) =>{
-                tripulantesAtuais.push(tripulante)
-                console.log(tripulantesAtuais)
-                rl.question("deseja adicionar outro tripulante? (s/n)", (res) => {
-                if(res.toLowerCase() =='s'){
-                    perguntarTripulantes(rl)
-                } else{
-                    const missao = { 
-                        nome: nomeAtual,
-                        destino: destinoAtual,
-                        prioridade: prioridadeAtual,
-                        tripulantes: tripulantesAtuais,
-                        concluida: false,
-                    }
-                    missoes.push(missao)
-                    console.log('Missão adicionada com sucesso!');
-                    console.log(typeof(menuF))
-                    menuF()
-                }
-                
-                }
-            )
-                } 
-                
-                )}
+function perguntarTripulantes(rl, nome, destino, prioridade, tripulantes, missoes, menu) {
+    rl.question("Adicionar um tripulante: ", (tripulante) => {
+        tripulantes.push(tripulante);
+        rl.question("Deseja adicionar outro tripulante? (s/n)", (res) => {
+            if (res.toLowerCase() === 's') {
+                perguntarTripulantes(rl, nome, destino, prioridade, tripulantes, missoes, menu);
+            } else {
+                const missao = {
+                    nome: nome,
+                    destino: destino,
+                    prioridade: prioridade,
+                    tripulantes: tripulantes,
+                    concluida: false,
+                };
+                missoes.push(missao);
+                console.log('Missão adicionada com sucesso!');
+                menu(rl);
+            }
+        });
+    });
+}
 
-module.exports = adicionarMissaoAoGerenciador
-
-
+module.exports = adicionarMissaoAoGerenciador;
