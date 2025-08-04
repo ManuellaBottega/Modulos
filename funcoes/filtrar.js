3230370bb119982c05e4ec9d85653751f6b3e678
Junior Carlos <junior-carlosdemattos@hotmail.com>
2025-08-04 09:53:20 -0300

    Att. filtrar.js

--- funcoes/filtrar.js
+++ funcoes/filtrar.js
@@ -1,8 +1,8 @@
-function FiltrarPorPrioridade() { 
+function FiltrarPorPrioridade(rl, missoes, menu) { 
     if (missoes.length === 0) {
         console.log('Nenhuma missão registrada.');
         console.log('\nPressione Enter para retornar ao menu...');
-        return rl.question('', menu);
+        return rl.question('', () => menu(rl));
     }
 
     rl.question('Por qual nível de prioridade você deseja filtrar (1 a 5)? ', (filtroPrioridadeInput) => {
@@ -11,7 +11,7 @@ function FiltrarPorPrioridade() {
 
         if (isNaN(prioridadeDesejada) || prioridadeDesejada < 1 || prioridadeDesejada > 5) {
             console.log('Prioridade inválida! Por favor, digite um número entre 1 e 5.');
-            return FiltrarPorPrioridade(); 
+            return FiltrarPorPrioridade(rl, missoes, menu); 
         }
 
         let missoesFiltradas = [];
@@ -26,7 +26,7 @@ function FiltrarPorPrioridade() {
         if (missoesFiltradas.length === 0) {
             console.log(`Não existe nenhuma missão no nível "${prioridadeDesejada}".`);
             console.log('\nPressione Enter para retornar ao menu...');
-            rl.question('', menu);
+            rl.question('', () => menu(rl));
         } else {
             
             console.log(`\n=== MISSÕES DE NÍVEL DE PRIORIDADE "${prioridadeDesejada}" ===`);
@@ -34,7 +34,7 @@ function FiltrarPorPrioridade() {
                 console.log(`${index + 1}. Nome: ${missao.nome} | Destino: ${missao.destino} | Tripulantes: ${missao.tripulantes.join(', ')} | Prioridade: ${missao.prioridade}`);
             });
             console.log('\nPressione Enter para retornar ao menu...');
-            rl.question('', menu);
+            rl.question('', () => menu(rl));
         }
     });
 }
